b9599ec9aafedb1c5a8bac4729dcb204251a22ce
JuaniSO <juan.bocchi@sphereone.xyz>
2023-06-27 02:11:20 -0300

    adding logiic to delete old users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -21,13 +21,46 @@ router.get("/successRegister", async (req, res) => {
 });
 
 router.post("/premium/:uid", async (req, res) => {
-  const {uid} = req.params;
-  const user = await User.findById({_id: uid});
-  if(user){
+  const { uid } = req.params;
+  const user = await User.findById({ _id: uid });
+  if (user) {
     user.role = "premium";
     await user.save();
-    res.send({message: "User premium!"});
+    res.send({ message: "User premium!" });
   }
 })
+
+router.delete("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    let usersToDelete = [];
+
+    users.forEach(user => {
+      let lastUserLogoutDate = new Date(user.last_connection.logout_date);
+      let currentDate = new Date();
+
+      // Saco los primeros 30 dias desde la fecha actual
+      currentDate.setDate(currentDate.getDate() - 30);
+
+      // Comparo para ver si esta entre los 30 dias
+      let isWithin30Days = lastUserLogoutDate >= currentDate;
+
+      if (!isWithin30Days) {
+        usersToDelete.push(user._id);
+      }
+    });
+
+    if(usersToDelete[0]){
+      // Borro aquellos usuarios cuyo logout_date es mayor a 30 dias!
+      await User.deleteMany({ _id: { $in: usersToDelete } });
+  
+      res.json({ message: "Users deleted successfully." });
+    }
+    res.json({message: "All the users are within the 30 days!"})
+
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while deleting users." });
+  }
+});
 
 export default router;
